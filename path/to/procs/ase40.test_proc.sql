9749f69f587178800151fd2f3a1901398d2f95ce
ase40 <abhinav.setia@sky.uk>
2020-01-14 12:39:45 +0000

    Updated procs on 2020-01-14 12:39:44

--- path/to/procs/ase40.test_proc.sql
+++ path/to/procs/ase40.test_proc.sql
@@ -1,6 +1,6 @@
 create procedure "ase40"."test_proc"()
 begin
   drop table if exists "ase40"."t1";
-  select 2 as "id",'abc' as "name","getdate"() as "runtime"
+  select 3 as "id",'abc' as "name","getdate"() as "runtime"
     into "ase40"."t1"
 end
