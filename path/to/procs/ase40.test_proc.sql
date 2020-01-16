b7258ebff9c45e474fc78b3ecfcd82feb6664cb5
ase40 <57446656+ase40@users.noreply.github.com>
2020-01-16 15:44:08 +0000

    test change

--- path/to/procs/ase40.test_proc.sql
+++ path/to/procs/ase40.test_proc.sql
@@ -2,5 +2,5 @@ create procedure "ase40"."test_proc"()
 begin
   drop table if exists "ase40"."t1";
   select 3 as "id",'abc' as "name","getdate"() as "runtime"
-    into "ase40"."t1"
+    into "ase40"."t2"
 end
